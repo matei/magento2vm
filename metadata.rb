6f9eadc36cf747593ba4993e8badf176c1b80e30
Matei Stefanescu <mstefanescu@optaros.com>
2015-10-24 13:34:05 +0300

    updating version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'matei@doize.ro'
 license          'All rights reserved'
 description      'Installs/Configures magento2vm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.4.0'
 
 depends 'apt'
 depends 'apache2'
